1b8e25c694ff32ab9014b046151ba0ba4ef0246e
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-07-16 17:28:23 +0800

    set color of active router link

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -6,15 +6,21 @@
   >
     <router-link class="flex flex-col items-center" to="/pos">
       <span class="material-symbols-outlined mb-1">receipt_long</span>
-      POS
+      <span>POS</span>
     </router-link>
     <router-link class="flex flex-col items-center" to="/">
       <span class="material-symbols-outlined mb-1">inventory</span>
-      Inventory
+      <span>Inventory</span>
     </router-link>
     <router-link class="flex flex-col items-center" to="/pricelist">
       <span class="material-symbols-outlined mb-1">price_check</span>
-      Pricelist
+      <span>Pricelist</span>
     </router-link>
   </div>
 </template>
+
+<style scoped>
+.router-link-active {
+  color: blue;
+}
+</style>
