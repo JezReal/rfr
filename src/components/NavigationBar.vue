d8770cf82bde3928bf1265d0b5ffbad1e7fb7b6e
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-07-16 17:51:09 +0800

    implement ripple effect on button click

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,17 +2,17 @@
 
 <template>
   <div
-    class="flex flex-row h-1/6 shadow-2xl shadow-orange-700 justify-around items-center text-sm px-10"
+    class="flex flex-row h-1/6 shadow-2xl shadow-orange-700 justify-around items-center text-sm px-10 py-2"
   >
-    <router-link class="flex flex-col items-center" to="/pos">
+    <router-link class="flex flex-col items-center p-4" to="/pos" v-wave>
       <span class="material-symbols-outlined mb-1">receipt_long</span>
       <span>POS</span>
     </router-link>
-    <router-link class="flex flex-col items-center" to="/">
+    <router-link class="flex flex-col items-center p-4" to="/" v-wave>
       <span class="material-symbols-outlined mb-1">inventory</span>
       <span>Inventory</span>
     </router-link>
-    <router-link class="flex flex-col items-center" to="/pricelist">
+    <router-link class="flex flex-col items-center p-4" to="/pricelist" v-wave>
       <span class="material-symbols-outlined mb-1">price_check</span>
       <span>Pricelist</span>
     </router-link>
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,18 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router/router";
 import "./index.css";
+import VWave from "v-wave";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(VWave, {
+  color: "gray",
+  startingOpacity: 0.5,
+  easing: "ease-in",
+  duration: 0.2,
+});
 
 app.mount("#app");
