d5895826736320cb67c03bafbd44b6f1c5750ade
Jezreel Joshua <jezreeljoshuam@gmail.com>
2022-07-16 17:20:03 +0800

    create navbar layout

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex flex-row bg-indigo-500 h-1/5">
-    <div>
-      <router-link to="/pos">POS</router-link>
-    </div>
-    <div>
-      <router-link to="/">Inventory</router-link>
-    </div>
-    <div>
-      <router-link to="/pricelist">Pricelist</router-link>
-    </div>
+  <div
+    class="flex flex-row h-1/6 shadow-2xl shadow-orange-700 justify-around items-center text-sm px-10"
+  >
+    <router-link class="flex flex-col items-center" to="/pos">
+      <span class="material-symbols-outlined mb-1">receipt_long</span>
+      POS
+    </router-link>
+    <router-link class="flex flex-col items-center" to="/">
+      <span class="material-symbols-outlined mb-1">inventory</span>
+      Inventory
+    </router-link>
+    <router-link class="flex flex-col items-center" to="/pricelist">
+      <span class="material-symbols-outlined mb-1">price_check</span>
+      Pricelist
+    </router-link>
   </div>
 </template>
